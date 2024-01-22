1c408183a5eeedea8d561cc6f9abfa1bbceeb267
Skip Montanaro <skip.montanaro@gmail.com>
2024-01-22 15:12:54 -0600

    tweak doc string

--- pyundoc/pyundoc.py
+++ pyundoc/pyundoc.py
@@ -2,10 +2,14 @@
 
 """Compare module contents with doc items.
 
+This script attempts to answer a very simple question. For any module M, which
+of the globally visible attributes don't appear as references of some kind
+(references in the Sphinx sense) in the module's documentation?
+
 1. Extract list of modules from local htmllive server.
-2. Open Sphinx inventory and reorganize slightly.
-3. Compare module globals with extracted refs and note any globals which appear to
-   be missing from the doc file.
+2. Load Sphinx inventory and reorganize slightly.
+3. Compare module globals with extracted refs and note any globals which appear
+to be missing from the doc file.
 """
 
 import argparse
